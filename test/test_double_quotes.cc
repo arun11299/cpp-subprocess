ab2d49a4451e8bd8767a8b6ac835b5e4e7e90bd2
Haowen Liu <lhw@lunacd.com>
2025-05-04 10:41:39 -0400

    Update include paths in tests

--- test/test_double_quotes.cc
+++ test/test_double_quotes.cc
@@ -1,4 +1,4 @@
-#include <subprocess.hpp>
+#include <cpp-subprocess/subprocess.hpp>
 
 #include <cassert>
 #include <string>
--- test/test_exception.cc
+++ test/test_exception.cc
@@ -1,6 +1,6 @@
 #include <cassert>
 #include <cstring>
-#include <subprocess.hpp>
+#include <cpp-subprocess/subprocess.hpp>
 
 namespace sp = subprocess;
 
