35b45716d0ff6ace88b2263fdebf975417ea158a
fanquake <fanquake@gmail.com>
2025-09-01 16:46:06 +0100

    Remove tab whitespace

--- cpp-subprocess/subprocess.hpp
+++ cpp-subprocess/subprocess.hpp
@@ -1584,7 +1584,7 @@ inline void Popen::execute_process() noexcept(false)
                             NULL,         // process security attributes
                             NULL,         // primary thread security attributes
                             TRUE,         // handles are inherited
-                            creation_flags,	// creation flags
+                            creation_flags, // creation flags
                             environment_string_table_ptr,  // use provided environment
                             cwd_arg,      // use provided current directory
                             &siStartInfo, // STARTUPINFOW pointer
